92d15152cc3803796910ee1439b44c2835c81f13
Emmar Caber <caberemmar@gmail.com>
2023-06-21 18:28:23 +0800

    Add some comments to understand better the code and reference

--- Environment-Setup/UnderstandingTheCode/main.py
+++ Environment-Setup/UnderstandingTheCode/main.py
@@ -1,10 +1,18 @@
+# importing the components we need
 from PySide6.QtWidgets import QApplication, QWidget
 
+# sys is a module responsible for processing command-line arguments
 import sys
 
+# the arguments inside the command-line
+# will be processed by the QApplication
 app = QApplication(sys.argv)
 
+# create a new window
+# NOTE: By default the windows or widget of QT are hidden,
+#       so you need to show it
 window = QWidget()
 window.show()
 
+# start the application
 app.exec()
