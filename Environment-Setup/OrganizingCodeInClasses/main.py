42c50cfd766f9739610e9c2a0eacd9f2b9ee760b
Emmar Caber <caberemmar@gmail.com>
2023-06-21 19:06:53 +0800

    Move the ButtonHolder class on a separate file

--- Environment-Setup/OrganizingCodeInClasses/main.py
+++ Environment-Setup/OrganizingCodeInClasses/main.py
@@ -40,3 +40,14 @@ window = ButtonHolder()
 window.show()
 app.exec()
 """
+
+import sys
+from PySide6.QtWidgets import QApplication
+from button_holder import ButtonHolder
+
+app = QApplication(sys.argv)
+
+window = ButtonHolder()
+window.show()
+
+app.exec()
