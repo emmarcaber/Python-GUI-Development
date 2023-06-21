bd27fb20f17912e22fec8429ce4c53fa87de19bf
Emmar Caber <caberemmar@gmail.com>
2023-06-21 19:00:15 +0800

    Create separate class for better organization of code

--- Environment-Setup/OrganizingCodeInClasses/main.py
+++ Environment-Setup/OrganizingCodeInClasses/main.py
@@ -1,17 +1,39 @@
 # VERSION1  :   Setting everything up in the global scope
+# from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+# import sys
+
+# app = QApplication(sys.argv)
+
+# window = QMainWindow()
+# window.setWindowTitle("Our first MainWindow App")
+
+# button = QPushButton()
+# button.setText("Press Me")
+
+# window.setCentralWidget(button)
+
+# window.show()
+# app.exec()
+
+
+# VERSION2  :   Setting up a separate class
+import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-import sys
+
+# Subclass QMainWindow to customize your application's window
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder app")
+        button = QPushButton("Press Me!")
+
+        # Set our button as the central widget
+        self.setCentralWidget(button)
+
 
 app = QApplication(sys.argv)
-
-window = QMainWindow()
-window.setWindowTitle("Our first MainWindow App")
-
-button = QPushButton()
-button.setText("Press Me")
-
-window.setCentralWidget(button)
-
+window = ButtonHolder()
 window.show()
 app.exec()
