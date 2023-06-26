b344cf7a6acb66f9e2edcfe17decdcff0fcd6e73
Emmar Caber <caberemmar@gmail.com>
2023-06-26 19:42:04 +0800

    Add capturing values from a slider in SignalAndSlots file

--- 2-SignalAndSlots/main.py
+++ 2-SignalAndSlots/main.py
@@ -29,6 +29,7 @@ app.exec()
 """
 
 # VERSION 2 :   SIGNALS SENDING VALUES, SLOTS CAPTURE THE VALUES
+"""
 from PySide6.QtWidgets import QApplication, QPushButton
 
 
@@ -47,4 +48,30 @@ button.setCheckable(True)  # Makes the button checkable. It's unchecked by defau
 button.clicked.connect(button_clicked)
 
 button.show()
+app.exec()
+"""
+
+
+# VERSION3  :   CAPTURE VALUE FROM A SLIDER - OTHER EXAMPLE
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QSlider
+
+
+# The slot  : responds when something happens
+def respond_to_slider(data):
+    print("slider moved to  :  ", data)
+
+
+app = QApplication()
+slider = QSlider(Qt.Vertical)
+slider.setMinimum(1)
+slider.setMaximum(100)
+slider.setValue(25)
+
+# You just do the connection. The Qt system takes care of
+# passing the data from the signal to the slot.
+slider.valueChanged.connect(respond_to_slider)
+slider.show()
+
 app.exec()
