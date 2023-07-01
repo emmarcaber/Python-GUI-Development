3cfc70c78816610add7b061fc39cc5959e68cc62
Emracabe <caberemmar@gmail.com>
2023-07-01 09:04:39 +0800

    Add new menus and actions in QMainWindow

--- 3-ATourOfQtWidgets/2-QMainWindow/mainwindow.py
+++ 3-ATourOfQtWidgets/2-QMainWindow/mainwindow.py
@@ -12,9 +12,24 @@ class MainWindow(QMainWindow):
     # Menubars and Menus
     def menubars_and_menus(self):        
         menubar = self.menuBar()
+
+        # NOTE: The ampersand enables the 
+        # pressing ALT+ the mnemonic character to set the focus to the control 
+        # that follows the Label in the tab order of the menus and MenuBar
         file_menu = menubar.addMenu("&File")
         quit_action = file_menu.addAction("Quit")
         quit_action.triggered.connect(self.quit_app)
+    
+        edit_menu = menubar.addMenu("&Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
+    
+        menubar.addMenu("&Window")
+        menubar.addMenu("&Setting")
+        menubar.addMenu("&Help")
 
     def quit_app(self):
         self.app.quit()
