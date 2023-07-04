a91a72375a651c0604a0c06aa52adf965e8196e6
emmarcaber <caberemmar@gmail.com>
2023-07-04 16:11:33 +0800

    Add some comments in QtDesigner utilizing QUiLoader

--- 4-QtDesigner/2-QtDesigner_QUiLoader/main.py
+++ 4-QtDesigner/2-QtDesigner_QUiLoader/main.py
@@ -3,16 +3,18 @@ import sys
 from PySide6 import QtWidgets
 from PySide6.QtUiTools import QUiLoader
 
-loader = QUiLoader()
+loader = QUiLoader()                        # Set up a loader object
 
 app = QtWidgets.QApplication(sys.argv)
-window = loader.load('widget.ui', None)
+window = loader.load('widget.ui', None)     # Load the UI - happens at run time!
 
 def do_something():
     print(f"{window.full_name_line_edit.text()} is a {window.occupation_line_edit.text()}")
 
+# Access the widgets in the form
 window.submit_button.clicked.connect(do_something)
 
+# Changes the properties of the window
 window.setWindowTitle("User Data")
 window.show()
 
