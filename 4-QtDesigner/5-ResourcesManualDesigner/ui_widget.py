3f4f2e3a1a3e3077eae620293c38fa67040ad18f
Emracabe <caberemmar@gmail.com>
2023-07-05 18:08:50 +0800

    Add ResourcesManualDesigner

---
+++ 4-QtDesigner/5-ResourcesManualDesigner/ui_widget.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'widget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QPushButton, QSizePolicy,
+    QSpinBox, QVBoxLayout, QWidget)
+import resource_rc
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(407, 60)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.minus_button = QPushButton(Form)
+        self.minus_button.setObjectName(u"minus_button")
+
+        self.horizontalLayout.addWidget(self.minus_button)
+
+        self.spin_box = QSpinBox(Form)
+        self.spin_box.setObjectName(u"spin_box")
+
+        self.horizontalLayout.addWidget(self.spin_box)
+
+        self.plus_button = QPushButton(Form)
+        self.plus_button.setObjectName(u"plus_button")
+
+        self.horizontalLayout.addWidget(self.plus_button)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.minus_button.setText(QCoreApplication.translate("Form", u"minus", None))
+        self.plus_button.setText(QCoreApplication.translate("Form", u"plus", None))
+    # retranslateUi
+
---
+++ 4-QtDesigner/5-ResourcesManualDesigner/widget.py
@@ -0,0 +1,41 @@
+"""
+NOTE:
+    You can generate the ready-made python file of .qrc file. By using the command:
+
+    ```
+        pyside6-rcc [filename.qrc] -o [filename-rc.py]
+    ```
+"""
+
+from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QIcon
+from ui_widget import Ui_Form
+import resource_rc
+
+class Widget(QWidget, Ui_Form):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+        self.setWindowTitle("User Data")
+
+        self.spin_box.setValue(50)
+
+        self.plus_button.clicked.connect(self.plus)
+        self.minus_button.clicked.connect(self.minus)
+
+        plus_icon = QIcon(":/images/plus.png")
+        minus_icon = QIcon(":/images/minus.png")
+
+        self.plus_button.setIcon(plus_icon)
+        self.minus_button.setIcon(minus_icon)
+
+    def plus(self):
+        value = self.spin_box.value()
+        self.spin_box.setValue(value + 1)
+
+    def minus(self):
+        value = self.spin_box.value()
+        self.spin_box.setValue(value - 1)
+
