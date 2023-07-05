273d41adf64def4a8590972d9b8ba1e535af563b
Emracabe <caberemmar@gmail.com>
2023-07-05 14:32:52 +0800

    Create form submission after pressing enter on QLineEdits

--- 4-QtDesigner/3-QtDesigner_QUiLoader_V2/user_interface.py
+++ 4-QtDesigner/3-QtDesigner_QUiLoader_V2/user_interface.py
@@ -9,6 +9,8 @@ class UserInterface(QtCore.QObject):
         self.ui = loader.load('widget.ui', None)
         self.ui.setWindowTitle("User Data")
         self.ui.submit_button.clicked.connect(self.do_something)
+        self.ui.occupation_line_edit.returnPressed.connect(self.do_something)
+        self.ui.full_name_line_edit.returnPressed.connect(self.do_something)
 
     def do_something(self):
             print(f"{self.ui.full_name_line_edit.text()} is a {self.ui.occupation_line_edit.text()}")
